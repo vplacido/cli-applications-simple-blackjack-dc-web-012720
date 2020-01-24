5bcbbe4d6924a18016766539ea4c2956c02a4d7b
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 22:25:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,21 +1,22 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand 1..11
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(cardTotal)
+  puts "Your cards add up to #{cardTotal}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  return gets.chomp
 end
 
 def end_game
